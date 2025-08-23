1404d40eecdc1ab3794ea21a330677e005918081
Timur <timson@Mac.lan>
2025-08-23 03:10:35 -0700

    seo

--- src/sections/main/Intro/Intro.tsx
+++ src/sections/main/Intro/Intro.tsx
@@ -5,9 +5,9 @@ const Intro = () => {
   return (
     <div className="flex items-center justify-center min-h-screen px-0.5 md:px-6 relative z-30">
       <div className="container">
-        <div className="w-full md:max-w-[50%] flex flex-col gap-10 z-30">
+        <div className="w-full md:max-w-[50%] flex flex-col gap-1 z-30">
           <IntroBlock />
-          <div className="flex text-center justify-center mt-20 sm:mt-0 sm:justify-start">
+          <div className="flex text-center justify-center  sm:mt-0 sm:justify-start">
             <LearnBtn />
           </div>
         </div>
--- src/sections/main/Main.tsx
+++ src/sections/main/Main.tsx
@@ -7,7 +7,7 @@ const Main = () => {
     <section className="w-screen h-screen bg-black relative overflow-hidden px-5  px-0.5 md:px-28 ">
       <MainBackground />
       <div
-        className="absolute z-500   bottom-[40px] left-1/2 -translate-x-1/2
+        className="absolute z-500   bottom-[100px] left-1/2 -translate-x-1/2
            md:bottom-[100px] md:right-[100px] md:left-auto md:translate-x-0
         "
       >
